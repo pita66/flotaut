5eef4c6c1fa0ac4953b03e9d968e21d996719b1d
piotr <piotr.paluch@outlook.com>
2022-01-10 23:57:20 +0100

    utworzona baza danych - Uzytkownik

--- flotaut/flotaut/settings.py
+++ flotaut/flotaut/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'jednostka_organizacyjna'
+    'jednostka_organizacyjna',
+    'uzytkownik'
 ]
 
 MIDDLEWARE = [
---
+++ flotaut/uzytkownik/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UzytkownikConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'uzytkownik'
