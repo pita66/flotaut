c43dcb8d07366d6bbdfaf8abc4c916c7c72d2583
piotr <piotr.paluch@outlook.com>
2022-01-11 00:11:54 +0100

    utworzona baza danych - Pojazd

--- flotaut/flotaut/settings.py
+++ flotaut/flotaut/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jednostka_organizacyjna',
-    'uzytkownik'
+    'uzytkownik',
+    'pojazd'
 ]
 
 MIDDLEWARE = [
--- flotaut/uzytkownik/models.py
+++ flotaut/uzytkownik/models.py
@@ -4,4 +4,5 @@ class Uzytkownik(models.Model):
     nazwisko = models.CharField(max_length=45)
     imie = models.CharField(max_length=45)
     id_pracownika = models.CharField(max_length=12)
-    jednostka_organizacyjna = models.ForeignKey('jednostka_organizacyjna.Jednostka_Organizacyjna', on_delete=models.CASCADE, null=True, blank=True)
+    jednostka_organizacyjna = models.ForeignKey('jednostka_organizacyjna.Jednostka_Organizacyjna',
+                                                on_delete=models.CASCADE, null=True, blank=True)
